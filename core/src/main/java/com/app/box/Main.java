ab84c771c886bd7d3bb545448ac7cfa6598151ab
Jeffrey MacPherson <jeff_macpherson95@hotmail.com>
2026-02-20 23:11:00 -0400

    added rock array, rock spawns

--- core/src/main/java/com/app/box/Main.java
+++ core/src/main/java/com/app/box/Main.java
@@ -2,12 +2,10 @@ package com.app.box;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 
@@ -19,10 +17,19 @@ public class Main extends ApplicationAdapter {
     private TextureRegion background;
     private Player player;
     private Array<Bullet> bullets;
+    private Array<Rock> rocks;
+    private float rockSpawnTimer;
+    private float rockSpawnInterval = 1.5f;
+    private static int MAX_ROCKS = 4;
+    private static float SCREEN_WIDTH;
+    private static float SCREEN_HEIGHT;
 
 
     @Override
     public void create() {
+        SCREEN_WIDTH = Gdx.graphics.getWidth();
+        SCREEN_HEIGHT = Gdx.graphics.getHeight();
+
         batch = new SpriteBatch();
         atlas = new TextureAtlas(Gdx.files.internal("atlas/textures.atlas"));
 
@@ -33,15 +40,30 @@ public class Main extends ApplicationAdapter {
         player = new Player(288.0f, 0.0f, playerRegion,  atlas, bulletRegion);
 
         bullets = new Array<>();
+        rocks = new Array<>();
     }
 
     @Override
     public void render() {
         float delta = Gdx.graphics.getDeltaTime();
+        rockSpawnTimer += delta;
 
         ScreenUtils.clear(0.0f, 0.0f, 0.0f, 1f);
 
         player.update(delta);
+
+        if (rockSpawnTimer >= rockSpawnInterval && rocks.size < MAX_ROCKS) {
+            spawnRock();
+            rockSpawnTimer = 0;
+        }
+
+        for (int i = rocks.size - 1; i >= 0; i--) {
+            Rock rock = rocks.get(i);
+            rock.update(delta);
+            if (!rock.isAlive) {
+                rocks.removeIndex(i);
+            }
+        }
 
         Bullet newBullet = player.shootBullet();
         if (newBullet != null) {
@@ -77,14 +99,30 @@ public class Main extends ApplicationAdapter {
 
         batch.begin();
 
-        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.draw(background, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
         player.render(batch);
 
         for (Bullet bullet : bullets) {
             bullet.render(batch);
         }
+        for (Rock rock : rocks) {
+            rock.render(batch);
+        }
 
         batch.end();
+    }
+
+    private void spawnRock() {
+        RockSize[] sizes = RockSize.values();
+        RockSize randomSize = sizes[(int)(MathUtils.random() * sizes.length)];
+
+        Rock temp = new Rock(0, 0, randomSize, atlas);
+
+        float maxX = SCREEN_WIDTH - temp.width;
+        float randomX = MathUtils.random() * maxX;
+        float spawnY = SCREEN_HEIGHT;
+
+        rocks.add(new Rock(randomX, spawnY, randomSize, atlas));
     }
 
     @Override
